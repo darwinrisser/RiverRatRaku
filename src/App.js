690f0af097d58a0def282d537b05bec9e03e9206
Darwin <risser.darwin@gmail.com>
2018-12-20 08:48:39 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './AppBar.js'
+import ImageGridList from './ImageGridList.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="home">
+        <AppBar/>
+          <h1>River Rat Raku</h1>
+        </div>
+        <div id="about" role="main">
+          <h1>Father, Husband, Potter</h1>
+          <div id="bio">
+            <p>
+              Jeffrey Lowrance currently works out of his studio in New Braunfels, Texas. He has a passion for designing unique and interesting pieces and loves to share his talent with family and friends.
+            </p>
+            <p>
+              Jeff has over four decades of experience working with ceramics. Much of his more recent work has explored <a href="https://en.wikipedia.org/wiki/Raku_ware">Raku</a> pottery. Originally a Japanese method, Western Raku became popular in the 1950s and quickly developed into a unique and distinctive style.
+            </p>
+            <p>
+              Raku involves the use of various waxes and glazes. Horse hair is often used to create fine lines and textures that give each piece a distinct look. You can see these styles in Jeff’s work.
+            </p>
+            <p>
+              Please take a look at our the gallery and if you are interested in purchasing any of Jeff's pieces, use the contact form below to get in touch with him.
+            </p>
+        </div>
+        </div>
+        <div id="gallery">
+          <h1>Gallery</h1>
+          <ImageGridList/>
+        </div>
+        <div id="contact">
+          alskdjfl;aksdjfl;akjsdf
+        </div>
       </div>
     );
   }
---
+++ src/tileData.js
@@ -0,0 +1,35 @@
+const tileData = [
+    {
+      img: "https://i.imgur.com/eoB5ZWP.jpg",
+      title: 'Image',
+      author: 'author',
+      cols: 2,
+    },
+    {
+        img: "https://i.imgur.com/oce6Bkw.jpg",
+        title: 'image',
+        author: 'author',
+        cols: 1,
+    },
+    {
+        img: "https://i.imgur.com/0AH3at3.jpg",
+        title: 'image',
+        author: 'author',
+        cols: 1,
+    },
+    {
+        img: "https://i.imgur.com/Vbe4bEt.jpg",
+        title: 'image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: "https://i.imgur.com/y3ON87d.jpg",
+        title: 'image',
+        author: 'author',
+        cols: 3,
+    },
+ ];
+
+
+ export default tileData;
